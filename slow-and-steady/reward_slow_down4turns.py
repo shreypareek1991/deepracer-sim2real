682209fef8f5716d197d2345e575f13cba2bea32
Shrey Pareek <shreypareek1991@gmail.com>
2024-08-26 10:15:38 -1000

    Create reward_slow_down4turns.py

---
+++ slow-and-steady/reward_slow_down4turns.py
@@ -0,0 +1,25 @@
+def reward_function(params):
+    '''
+    Example of rewarding the agent to slow down for turns
+    '''
+    reward = 1e-3
+    
+    # fast on straights and slow on curves
+    steering_angle = params['steering_angle']
+    speed = params['speed']
+
+    # set a steering threshold above which angles are considered large
+    # you can change this based on your action space
+    STEERING_THRESHOLD  = 15
+    
+    if abs(steering_angle) > STEERING_THRESHOLD:
+        if speed < 1:
+            # slow speeds are awarded large positive rewards
+            reward += 2.0
+        elif speed < 2:
+            # faster speeds are awarded smaller positive rewards
+            reward += 0.5
+        # reduce zigzagging behavior by penalizing large steering angles
+        reward *= 0.85
+        
+    return float(reward)
